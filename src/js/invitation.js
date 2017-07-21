861c7dec866b5caf0f8d1b9a4854602e201b5797
xtwang <xtwang@thoughtworks.com>
2017-07-21 14:25:15 +0800

    add js to handle page click

--- src/js/invitation.js
+++ src/js/invitation.js
@@ -1,2 +1,38 @@
 require('../../index.html')
 require('../style/invitation.scss')
+
+var $availableNumber = document.getElementById('availableNumber')
+var $users = document.getElementsByName('user')
+var $eMail = document.getElementById('eMail')
+var $main = document.getElementsByTagName('main')[0]
+var $invite = document.getElementById('invite')
+
+function calcAvailableNumber() {
+  var $selectedUsers = document.querySelectorAll('input[name="user"]:checked')
+  $availableNumber.innerText = $users.length - $selectedUsers.length
+
+  var isNotAllow = !($selectedUsers.length || $eMail.value);
+  $invite.disabled = isNotAllow
+  $invite.className = isNotAllow ? 'disabled' : ''
+}
+
+$main.addEventListener('click', function(evt) {
+  if(evt.target.type !== 'checkbox') return
+  calcAvailableNumber()
+})
+
+$invite.addEventListener('click', function() {
+  var message = ''
+
+  document.querySelectorAll('input[name="user"]:checked').forEach(function(user) {
+    message += user.value + ' '
+  })
+
+  alert(message + $eMail.value + ' has been invited')
+})
+
+$eMail.addEventListener('keyup', function() {
+  calcAvailableNumber()
+})
+
+calcAvailableNumber()
